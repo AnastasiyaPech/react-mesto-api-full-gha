ff710c7a71afda0c533b2a379064225dbbcc360e
Anastasiya Pechnikova <anastasiyaverty@yandex.ru>
2023-08-31 00:55:21 +0300

    Fixes

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -9,6 +9,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
+    .then((card) => card.populate('owner'))
     .then((card) => {
       res.status(201).send(card);
     })
